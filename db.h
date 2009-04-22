846ec1f604638ebb0c0a54f2c6b59441b3c28f03
eungyu <eungyu@celes.kgfamily.com>
2009-04-22 03:34:54 -0400

    - added comments

--- db.h
+++ db.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <time.h>
 
-#define QUERY_PAD   (32)
-#define SYMBOL_PAD   (4)
-#define RFCDATE_PAD (32)
+#define QUERY_PAD   (32)  /* extra room for query keywords    */
+#define SYMBOL_PAD   (4)  /* extra room for quotes and commas */
+#define RFCDATE_PAD (32)  /* extra room for rfc date string   */
 
 typedef sqlite3 clog_db;
 
@@ -18,16 +18,18 @@ typedef enum {
 } mod_type;
 
 struct clog_transaction {
-	mod_type type;
-	unsigned int id;
-	char *title;
-	char *content;
-	time_t time;
+	mod_type type;     /* transaction type                   */
+	unsigned int id;   /* only used in UPDATE/DELETE         */
+	char *title;       /* pointer to title buffer            */
+	char *content;     /* pointer to content buffer          */
+	time_t time;       /* c_time or u_time depending on type */
 };
 
+/* clog_db open/close functions */
 clog_db *clog_db_open(const char *name);
 void clog_db_close(clog_db *db);
 
+/* clog_db manipulation functions */
 int 
 clog_modify_db(
 	clog_db *database_handle, 
